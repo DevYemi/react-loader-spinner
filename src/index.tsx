b2d7a4e0a0336f18fcdb8b43785c530fe770b127
Mohan <mhnpd@outlook.com>
2022-02-06 19:22:36 +0100

    WIP

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import BallTriangle from './loader/BallTriangle'
 import Bars from './loader/Bars'
 import Circles from './loader/Circles'
 import CirclesWithBar from './loader/CirclesWithBar'
-import CoffeeCup from './loader/CoffeeCup'
 import CradleLoader from './loader/CradleLoader'
 import Grid from './loader/Grid'
 import Hearts from './loader/Hearts'
@@ -28,7 +27,6 @@ export {
   CradleLoader,
   Circles,
   CirclesWithBar,
-  CoffeeCup,
   Grid,
   Hearts,
   LineWave,
